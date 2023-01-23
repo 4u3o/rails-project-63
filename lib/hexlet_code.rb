4c101c9346e96c9c6fdc4a6f3807260ee0cb35a0
Anton Goroshko <radjahair@gmail.com>
2023-01-23 18:32:13 +1000

    Add HexletCode::form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,9 +2,12 @@
 
 require_relative "hexlet_code/version"
 
+# Hexlet homework.
 module HexletCode
-  # Tag
-  class Tag
+  User = Struct.new(:name, :job, keyword_init: true)
+
+  # Tag generates html tags.
+  module Tag
     SINGLE_TAGS = %w[br hr input img].freeze
 
     def self.build(tag_name, **attrs, &block)
@@ -18,5 +21,9 @@ module HexletCode
 
       opening_tag + text + closing_tag
     end
+  end
+
+  def self.form_for(_user, url: "#", &_block)
+    "<form action=\"#{url}\" method=\"post\"></form>"
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,6 +3,10 @@
 require "test_helper"
 
 class TestHexletCode < Minitest::Test
+  def setup
+    @user = HexletCode::User.new name: "rob"
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
@@ -31,6 +35,24 @@ class TestHexletCode < Minitest::Test
   def test_that_build_returns_tag_without_attrs_and_text
     expected = "<div></div>"
     result = HexletCode::Tag.build("div")
+
+    assert_equal expected, result
+  end
+
+  def test_that_form_for_returns_form
+    expected = "<form action=\"#\" method=\"post\"></form>"
+
+    result = HexletCode.form_for @user do |f|
+    end
+
+    assert_equal expected, result
+  end
+
+  def test_that_form_for_returns_form_with_action
+    expected = "<form action=\"/users\" method=\"post\"></form>"
+
+    result = HexletCode.form_for @user, url: "/users" do |f|
+    end
 
     assert_equal expected, result
   end
