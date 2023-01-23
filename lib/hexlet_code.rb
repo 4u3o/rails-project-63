09ed89c3d452b6a331be6a43c525e2663fdbd97d
Anton Goroshko <radjahair@gmail.com>
2023-01-23 12:32:56 +1000

    Add Tag#build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,6 +3,19 @@
 require_relative "hexlet_code/version"
 
 module HexletCode
-  class Error < StandardError; end
-  # Your code goes here...
+  class Tag
+    SINGLE_TAGS = %w[br hr input img].freeze
+
+    def self.build(tag_name, **attrs, &block)
+      attrs_str = attrs.each_with_object([]) do |(k, v), acc|
+        acc << " #{k}=\"#{v}\""
+      end.join
+
+      opening_tag = "<#{tag_name}#{attrs_str}>"
+      text = block_given? ? block.call : ""
+      closing_tag = SINGLE_TAGS.include?(tag_name) ? "" : "</#{tag_name}>"
+
+      opening_tag + text + closing_tag
+    end
+  end
 end
