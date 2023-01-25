7aa0245b869820df02d17ea380c19e648d7250e1
Anton Goroshko <radjahair@gmail.com>
2023-01-25 18:45:49 +1000

    Add Form#input

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,26 +4,51 @@ require_relative "hexlet_code/version"
 
 # Hexlet homework.
 module HexletCode
-  User = Struct.new(:name, :job, keyword_init: true)
+  User = Struct.new(:name, :job, :gender, keyword_init: true)
+  Form = Struct.new("Form", :for_obj, :opening_tag, :content, :closing_tag) do |_|
+    def to_s
+      opening_tag + content + closing_tag
+    end
+
+    def input(name, type: "text", cols: "20", rows: "40", **attrs)
+      value = for_obj.public_send(name)
+
+      self.content += (
+        if attrs[:as] == :text
+          Tag.build("textarea", name:, cols:, rows:) do
+            value
+          end
+        else
+          Tag.build("input", name:, type:, value:, **attrs)
+        end
+      )
+    end
+  end
 
   # Tag generates html tags.
   module Tag
     SINGLE_TAGS = %w[br hr input img].freeze
 
-    def self.build(tag_name, **attrs, &block)
+    def self.create(tag_name, **attrs, &block)
       attrs_str = attrs.each_with_object([]) do |(k, v), acc|
         acc << " #{k}=\"#{v}\""
       end.join
 
       opening_tag = "<#{tag_name}#{attrs_str}>"
-      text = block_given? ? block.call : ""
+      content = block_given? ? block.call : ""
       closing_tag = SINGLE_TAGS.include?(tag_name) ? "" : "</#{tag_name}>"
 
-      opening_tag + text + closing_tag
+      [opening_tag, content, closing_tag]
+    end
+
+    def self.build(tag_name, **attrs, &block)
+      create(tag_name, **attrs, &block).join
     end
   end
 
-  def self.form_for(_user, url: "#", &_block)
-    "<form action=\"#{url}\" method=\"post\"></form>"
+  def self.form_for(obj, url: "#", method: "post", &block)
+    form = Form.new(obj, *Tag.create("form", action: url, method:))
+    block.call(form)
+    form.to_s
   end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -4,11 +4,7 @@ require "test_helper"
 
 class TestHexletCode < Minitest::Test
   def setup
-    @user = HexletCode::User.new name: "rob"
-  end
-
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
+    @user = HexletCode::User.new name: "rob", job: "hexlet"
   end
 
   def test_that_build_returns_single_tag
@@ -55,5 +51,44 @@ class TestHexletCode < Minitest::Test
     end
 
     assert_equal expected, result
+  end
+
+  def test_that_form_for_returns_form_with_block
+    fixture = File.new(
+      "#{__dir__}/fixtures/form_with_input_and_textaria.html", "r"
+    )
+    expected = fixture.read
+    fixture.close
+
+    result = HexletCode.form_for @user do |f|
+      f.input :name, class: "user-input"
+      f.input :job, as: :text
+    end
+
+    assert_equal expected, result
+  end
+
+  def test_that_form_change_default_input_attrs
+    fixture = File.new(
+      "#{__dir__}/fixtures/form_with_changed_default_attrs.html"
+    )
+    expected = fixture.read
+    fixture.close
+
+    result = HexletCode.form_for @user, url: "#" do |f|
+      f.input :job, as: :text, rows: 50, cols: 50
+    end
+
+    assert_equal expected, result
+  end
+
+  def test_that_form_raise_error_if_user_didnt_have_attr
+    assert_raises NameError do
+      HexletCode.form_for @user, url: "/users" do |f|
+        f.input :name
+        f.input :job, as: :text
+        f.input :age
+      end
+    end
   end
 end
