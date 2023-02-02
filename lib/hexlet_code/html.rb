b93d3da77c5e938693e4fc5df595d196586e6838
Anton Goroshko <radjahair@gmail.com>
2023-02-02 18:46:53 +1000

    Fix uninitialized constant HTML::Tag

--- lib/hexlet_code/html.rb
+++ lib/hexlet_code/html.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class HTML
+  autoload :Tag, 'hexlet_code/tag'
+
   def initialize(tag, elements, **attrs)
     @opening = Tag.create_opening_tag(tag, **attrs)
     @content = elements.map do |element|
